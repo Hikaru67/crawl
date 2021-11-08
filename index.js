5b132d622fbc6af44684aaee0d9262c9ce08788a
hikaru67 <shinigamii.hikaru@gmail.com>
2021-11-08 07:38:31 +0700

    add sv const

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const express = require('express')
+const app = express()
 const puppeteer = require('puppeteer');
+const comments = require('./const/comment')
+console.log("comments", comments)
+
+const PORT = process.env['PORT']
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-(async () => {
+app.get('/increase-like', async (req, res) => {
+  if (!req.query.id) {
+    res.status(422).json({ message: 'No query id' })
+  }
   const url = 'https://padlet.com/chithuong70/uahr2skx76gww651';
-  for (let i = 0; i < 86; i++) {
+  for (let i = 0; i < 321; i++) {
     try {
       puppeteer.clearCustomQueryHandlers()
       const browser = await puppeteer.launch();
@@ -14,8 +23,10 @@ function sleep(ms) {
       await page.goto(url);
 
       const test = await page.evaluate(async () => {
-        a = document.querySelectorAll('#wish-1872899933 section .cursor-pointer')[0];
+        b = document.querySelectorAll('#wish-1872646690 section .cursor-pointer')[0];
+        a = document.querySelectorAll('#wish-1872938257 section .cursor-pointer')[0];
         if (a) {
+          b.click();
           return a.click();
         }
         return false
@@ -31,4 +42,39 @@ function sleep(ms) {
       continue;
     }
   }
-})();
+  res.status(200).json('done')
+})
+
+
+  (async () => {
+    const url = 'https://padlet.com/chithuong70/uahr2skx76gww651';
+    for (let i = 0; i < 321; i++) {
+      try {
+        puppeteer.clearCustomQueryHandlers()
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+
+        const test = await page.evaluate(async () => {
+          b = document.querySelectorAll('#wish-1872646690 section .cursor-pointer')[0];
+          a = document.querySelectorAll('#wish-1872938257 section .cursor-pointer')[0];
+          if (a) {
+            b.click();
+            return a.click();
+          }
+          return false
+        });
+        console.log("test => test", test)
+
+        await browser.close();
+        await sleep(Math.floor(Math.random() * 50));
+
+        console.log(i);
+      } catch (error) {
+        console.log("error", error)
+        continue;
+      }
+    }
+  })();
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
