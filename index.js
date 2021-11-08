77c4f3fb35931a9c8f56111af7ba33bca3446574
hikaru67 <shinigamii.hikaru@gmail.com>
2021-11-08 07:00:20 +0700

    ud

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ function sleep(ms) {
 
 (async () => {
   const url = 'https://padlet.com/chithuong70/uahr2skx76gww651';
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 86; i++) {
     try {
       puppeteer.clearCustomQueryHandlers()
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
       await page.goto(url);
 
-      const test = await page.evaluate(() => {
-        a = document.querySelectorAll('#wish-1872938257 section .cursor-pointer')[0];
-        return a.click();
+      const test = await page.evaluate(async () => {
+        a = document.querySelectorAll('#wish-1872899933 section .cursor-pointer')[0];
+        if (a) {
+          return a.click();
+        }
+        return false
       });
       console.log("test => test", test)
 
       await browser.close();
-      await sleep(500);
+      await sleep(Math.floor(Math.random() * 50));
 
       console.log(i);
     } catch (error) {
