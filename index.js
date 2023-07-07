4335abf5af72d51bcb1664adcb37fe2110d5d84a
quangnn73 <quangnn73@ghtk.co>
2023-07-07 16:31:16 +0700

    format

--- index.js
+++ index.js
@@ -6,45 +6,39 @@ const PORT = process.env.PORT || 7200
 
 async function getPhone(url) {
   try {
-    puppeteer.clearCustomQueryHandlers()
     var btnCickClass = '.ShowPhoneButton_phoneButton__p5Cvt.ShowPhoneButton_phoneNotClicked__dlQn_'
     var phoneSpanClass = '.ShowPhoneButton_phoneButton__p5Cvt.ShowPhoneButton_phoneClicked__IxuR6 span'
     const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
-    await page.setViewport({ width: 2920, height: 1080 });
-    const ua =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36";
-    await page.setUserAgent(ua);
-    await page.setRequestInterception(true);
+    const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36';
+    await Promise.all([
+      page.setViewport({ width: 2920, height: 1080 }),
+      page.setUserAgent(ua),
+      page.setRequestInterception(true)
+    ])
 
     page.on('request', (req) => {
-        if(req.resourceType() == 'font' || req.resourceType() == 'image'){
-          req.abort();
-        }
-        else {
-            req.continue();
-        }
+      if (req.resourceType() == 'font' || req.resourceType() == 'image') {
+        req.abort();
+      }
+      else {
+        req.continue();
+      }
     });
 
-    console.log('goto url')
-    try {
-      await page.goto(url, { waitUntil: "networkidle2" })
-      let element = await page.waitForSelector(btnCickClass, { visible: true })
-      await page.evaluate(el => el.click(), element)
+    await page.goto(url, { waitUntil: "networkidle2" })
+    let element = await page.waitForSelector(btnCickClass, { visible: true })
+    await page.evaluate(el => el.click(), element)
 
-      
-      let phone = null
-      while (!phone || phone.indexOf('*') >= 0) {
-        element = await page.waitForSelector(phoneSpanClass)
-        phone = await page.evaluate(el => el.textContent, element)
-      }
-
-      await browser.close()
-
-      return phone
-    } catch (error) {
+    let phone = null
+    while (!phone || phone.indexOf('*') >= 0) {
+      element = await page.waitForSelector(phoneSpanClass)
+      phone = await page.evaluate(el => el.textContent, element)
     }
-    
+
+    await browser.close()
+
+    return phone;
   } catch (error) {
     console.log("error", error)
     return 'error'
@@ -61,12 +55,12 @@ app.get('/get-phone', async (req, res) => {
   res.status(200).json({ [url]: phone })
 })
 
-app.get('/get-phones', async (req, res) => {  
+app.get('/get-phones', async (req, res) => {
   if (!req.query.urls) {
     res.status(422).json({ message: 'No query id or number' })
   }
   const urls = req.query.urls
-  const data = await Promise.all(urls.map(async (url) => { return { [url]: await getPhone(url) }}))
+  const data = await Promise.all(urls.map(async (url) => { return { [url]: await getPhone(url) } }))
 
   res.status(200).json(data)
 })
